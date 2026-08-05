85710047d7468f570391f0b25b11e837bad815a1
AlexanderGG <95066427+AlexanderGG-0520@users.noreply.github.com>
2026-08-05 14:20:45 +0900

    Route outline material providers to replacement service

--- src/backport1211/client/java/dev/alex/threadium/mixin/OutlineVertexConsumerProviderMixin.java
+++ src/backport1211/client/java/dev/alex/threadium/mixin/OutlineVertexConsumerProviderMixin.java
@@ -1,5 +1,6 @@
 package dev.alex.threadium.mixin;
 
+import dev.alex.threadium.render.entity.ModelPartReplacementService;
 import dev.alex.threadium.render.entity.PassThroughEntityRenderService;
 import dev.alex.threadium.render.modelpart.material.MaterialProviderSource;
 import net.minecraft.client.render.OutlineVertexConsumerProvider;
@@ -19,7 +20,10 @@ abstract class OutlineVertexConsumerProviderMixin {
             require = 1)
     private void threadium$observeReturnedConsumer(
             RenderLayer layer, CallbackInfoReturnable<VertexConsumer> callbackInfo) {
+        VertexConsumer consumer = callbackInfo.getReturnValue();
         PassThroughEntityRenderService.observeMaterialProviderRequest(
-                MaterialProviderSource.OUTLINE, this, layer, callbackInfo.getReturnValue());
+                MaterialProviderSource.OUTLINE, this, layer, consumer);
+        ModelPartReplacementService.observeMaterialProviderRequest(
+                MaterialProviderSource.OUTLINE, this, layer, consumer);
     }
 }
